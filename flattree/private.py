4e8966722256a1e28e84e1fe761e928676b0d1a5
Aleksandr Mikhailov <dev@avidclam.com>
2021-01-14 14:43:54 +0300

    Add xtree flattining code into private.py, docstrings/typing to follow

---
+++ flattree/private.py
@@ -0,0 +1,90 @@
+from collections.abc import Hashable
+from .settings import get_symbols
+
+
+class Leaf:
+    def __init__(self, value):
+        self.value = value
+
+    def render(self):
+        return self.value
+
+
+class Key:
+    def __init__(self, key):
+        self.key = key
+
+
+class ListKey(Key):
+    def __init__(self, key):
+        try:
+            super().__init__(int(key))
+        except ValueError:
+            super().__init__(key)
+
+    def is_valid(self):
+        return isinstance(self.key, int)
+
+    def __repr__(self):
+        return f"ListKey({repr(self.key)})"
+
+
+class DictKey(Key):
+    def is_valid(self):
+        return isinstance(self.key, Hashable)
+
+    def __repr__(self):
+        return f"DictKey({repr(self.key)})"
+
+
+def gen_leaf_tuples(xtree, preface=None):
+    if preface is None:
+        preface = []
+    if isinstance(xtree, dict) and xtree:
+        for key, value in xtree.items():
+            yield from gen_leaf_tuples(xtree[key], preface + [DictKey(key)])
+    elif isinstance(xtree, list) and xtree:
+        for i, el in enumerate(xtree):
+            yield from gen_leaf_tuples(el, preface + [ListKey(i)])
+    else:
+        yield preface, xtree
+
+
+def encode_step(step: Key, settings):
+    sep, lbr, rbr, lquo, rquo = get_symbols(settings)
+    if isinstance(step, ListKey):
+        return f"{lbr}{step.key}{rbr}"
+    special = set(f"{sep}{lbr}{rbr}{lquo}{rquo}")
+    if isinstance(step.key, str):
+        key_str = step.key
+        if (
+                key_str.isdigit()
+                or key_str in ('None', 'False', 'True')
+                or set(key_str) & special
+        ):
+            replaced = key_str.replace(lquo, lquo * 2)
+            if lquo != rquo:
+                replaced = replaced.replace(rquo, rquo * 2)
+            return f"{lquo}{replaced}{rquo}"
+        else:
+            return key_str
+    return str(step.key)
+
+
+def gen_crown(xtree, settings, preface=None):
+    sep, lbr, *_ = get_symbols(settings)
+    for path, value in gen_leaf_tuples(xtree, preface=preface):
+        if path:
+            leafkey = encode_step(path[0], settings=settings)
+            for step in path[1:]:
+                key_str = encode_step(step, settings=settings)
+                if not key_str.startswith(lbr):
+                    key_str = sep + key_str
+                leafkey += key_str
+        else:
+            leafkey = None
+        yield leafkey, value
+
+
+def build_crown(xtree, settings, preface=None):
+    return dict(gen_crown(xtree=xtree, settings=settings, preface=preface))
